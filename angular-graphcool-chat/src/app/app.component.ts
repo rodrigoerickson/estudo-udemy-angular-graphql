ed9ba8d8d63bf3109532168855019210859aeb43
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-08-29 13:33:22 -0300

    S3C26 - query simples com GraphQL e HttpClient

--- angular-graphcool-chat/src/app/app.component.ts
+++ angular-graphcool-chat/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-graphcool-chat';
+
+  private apiUrl = 'https://api.graph.cool/simple/v1/cjzsyt38x0xuy0103ba7jy6qe';
+
+  constructor(
+    private http: HttpClient
+  ){
+    this.allUsers();
+  }
+
+  allUsers():void{
+    const body = {
+      query: 
+      `
+      query {
+        allUsers{
+          id
+          name
+          email
+        }
+      }
+      `
+    }
+
+    this.http.post(this.apiUrl, body)
+    .subscribe(res => console.log(res))
+  }
 }
