16306fc4dcbad8d033148d1b86fd7b1ffb625919
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-10-13 17:19:28 -0300

    S7C89 - AthGuard para rotas protegidas: CanActivateChild

--- angular-graphcool-chat/src/app/login/auth.guard.ts
+++ angular-graphcool-chat/src/app/login/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, CanActivateChild } from '@angular/router';
 import { LoginRoutingModule } from './login-routing.module';
 import { AuthService } from '../core/services/auth.service';
 import { Observable } from 'rxjs';
@@ -7,12 +7,16 @@ import { Observable } from 'rxjs';
 @Injectable(
     {providedIn: LoginRoutingModule}
 )
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
     constructor(
         private authService: AuthService
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):Observable<boolean> {
         return this.authService.isAuthenticate;
+    }
+
+    canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        return this.canActivate(route, state);
     }
 }
