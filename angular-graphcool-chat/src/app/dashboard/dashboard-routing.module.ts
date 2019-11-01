396a0bb58e7c5ef63c8eed35b6b106a9980d2fff
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-01 18:46:56 -0300

    S8c109 - Configurando a rota intera para ComponentHome

--- angular-graphcool-chat/src/app/dashboard/dashboard-routing.module.ts
+++ angular-graphcool-chat/src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path:'', component: DashboardHomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
