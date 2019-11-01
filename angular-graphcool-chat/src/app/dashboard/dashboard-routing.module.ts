e7ab4889e2a0d4b2a95b2f3eec84b19dce5651c0
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-01 18:49:10 -0300

    S8c110 - Protegendo a rota com AuthGuard no CanActivate

--- angular-graphcool-chat/src/app/dashboard/dashboard-routing.module.ts
+++ angular-graphcool-chat/src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+import { AuthGuard } from '../login/auth.guard';
 
 const routes: Routes = [
-    {path:'', component: DashboardHomeComponent}
+    { path:'', component: DashboardHomeComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
