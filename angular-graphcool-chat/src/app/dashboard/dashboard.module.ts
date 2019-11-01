f74dd0c13fde368a82bc48c316fd560da7ce57b1
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-01 18:35:56 -0300

    S8c107 - DashboardModule e DashboardRoutingModule

---
+++ angular-graphcool-chat/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    SharedModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
