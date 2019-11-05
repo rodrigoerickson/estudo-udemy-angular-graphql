570480affa0bd42ddd4b721cbe36e743cd753f5c
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-05 17:43:48 -0300

    S8c112 - Carregando DashBoardModule sob demanda com lazy loading

--- angular-graphcool-chat/src/app/app-routing.module.ts
+++ angular-graphcool-chat/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './login/auth.guard';
 
 const routes: Routes = [
+    {
+      path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule',
+      canLoad: [AuthGuard]  
+    },
   { path: '', redirectTo: 'login', pathMatch:'full'}
 ];
 
