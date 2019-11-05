7cc5c5611034e573b9a112f3781fdc1132b2821b
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-05 20:03:30 -0300

    S8c116 - Componente para exibição de links para outros moduloes

--- angular-graphcool-chat/src/app/dashboard/dashboard.module.ts
+++ angular-graphcool-chat/src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
+import { DashboardResourcesComponent } from './components/dashboard-resources/dashboard-resources.component';
 
 @NgModule({
-  declarations: [DashboardHomeComponent, DashboardHeaderComponent],
+  declarations: [DashboardHomeComponent, DashboardHeaderComponent, DashboardResourcesComponent],
   imports: [
     SharedModule,
     DashboardRoutingModule
