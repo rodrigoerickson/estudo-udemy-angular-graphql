30506f8892641c7ef3b50b0f81c772f04159c4fb
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-05 19:35:52 -0300

    S8c114 - Component header e logout para a aplicação

---
+++ angular-graphcool-chat/src/app/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Component({
+  selector: 'app-dashboard-header',
+  templateUrl: './dashboard-header.component.html',
+  styleUrls: ['./dashboard-header.component.scss']
+})
+export class DashboardHeaderComponent implements OnInit {
+
+  constructor(
+      private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogout():void{
+    this.authService.logout();
+  }
+
+}
--- angular-graphcool-chat/src/app/dashboard/dashboard.module.ts
+++ angular-graphcool-chat/src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 
 @NgModule({
-  declarations: [DashboardHomeComponent],
+  declarations: [DashboardHomeComponent, DashboardHeaderComponent],
   imports: [
     SharedModule,
     DashboardRoutingModule
