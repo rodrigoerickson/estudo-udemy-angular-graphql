32b197a93812aecf2db6d72e635e64d79a5f1cbe
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-06 10:54:53 -0300

    S8c120 - Configurando rota filha para o DashboardResources

--- angular-graphcool-chat/src/app/dashboard/dashboard-routing.module.ts
+++ angular-graphcool-chat/src/app/dashboard/dashboard-routing.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '../login/auth.guard';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+import { DashboardResourcesComponent } from './components/dashboard-resources/dashboard-resources.component';
 
 const routes: Routes = [
-    { path:'', component: DashboardHomeComponent, canActivate:[AuthGuard] }
+    { 
+        path:'',
+        component: DashboardHomeComponent,
+        canActivate:[AuthGuard],
+        canActivateChild: [AuthGuard],
+        children:[
+            {
+                path:'',
+                component: DashboardResourcesComponent
+            }
+        ]
+    }
 ];
 
 @NgModule({
