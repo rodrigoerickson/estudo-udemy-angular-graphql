cd42c8b7e316d75436e1d41bc33a2f8fb877cf26
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-07 10:27:28 -0300

    S9c126 - Configurando rota interna para o ChatTabComponent

--- angular-graphcool-chat/src/app/chat/chat-routing.module.ts
+++ angular-graphcool-chat/src/app/chat/chat-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
+import { AuthGuard } from '../login/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: ChatTabComponent,
+        canActivate:[AuthGuard]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
