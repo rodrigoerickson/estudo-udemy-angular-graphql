0b2b7b0036b0cf602e1707432b05f0fbcc81e459
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-07 11:00:39 -0300

    S9c130 - Configurando rotas e router outlet para lista de usuários e chats

--- angular-graphcool-chat/src/app/chat/chat-routing.module.ts
+++ angular-graphcool-chat/src/app/chat/chat-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { AuthGuard } from '../login/auth.guard';
+import { ChatUsersComponent } from './components/chat-users/chat-users.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
 
 const routes: Routes = [
     {
         path: '',
         component: ChatTabComponent,
-        canActivate:[AuthGuard]
+        canActivate:[AuthGuard],
+        canActivateChild: [AuthGuard],
+        children: [
+            { path: 'users', component: ChatUsersComponent},
+            { path: '', component: ChatListComponent}
+        ]
     }
 ];
 
