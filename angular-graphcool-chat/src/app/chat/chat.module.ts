b8c76fec5d421e4889a20a008dd78869342831ba
Rodrigo Erickson <cin_rosilva@uolinc.com>
2019-11-07 10:37:49 -0300

    S9c128 - ChatUsersComponent para exibir lista de usuários do chat

--- angular-graphcool-chat/src/app/chat/chat.module.ts
+++ angular-graphcool-chat/src/app/chat/chat.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { ChatRoutingModule } from './chat-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
+import { ChatUsersComponent } from './components/chat-users/chat-users.component';
 
 @NgModule({
-  declarations: [ChatTabComponent],
+  declarations: [ChatTabComponent, ChatUsersComponent],
   imports: [
     SharedModule,
     ChatRoutingModule
